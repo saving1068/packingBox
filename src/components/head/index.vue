beada8601827c6bdd1941ab2db071677bf297cf5
saving <34386747+saving1068@users.noreply.github.com>
2020-01-05 22:06:39 +0800

    123

---
+++ src/components/head/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="warp">
+        <div>
+             <el-input
+             clearable
+            size="mini"
+            placeholder="请输入内容"
+            suffix-icon="el-icon-search"
+            v-model="searchValue">
+        </el-input>
+        </div>
+       
+       
+        <div class="login">
+             <i class="el-icon-bell"></i>
+            <div class="avatar">
+                <el-avatar  size="small" src="../../assets/logo.png"></el-avatar>
+            </div>
+            <el-dropdown szie="mini">
+            <span class="el-dropdown-link">
+                下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+<script>
+     export default{
+        data(){
+            return{
+                searchValue:""
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+
+    .warp{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 60px;
+        .avatar{
+            padding: 0 20px;
+        }
+        .login{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            .el-dropdown-link {
+                cursor: pointer;
+                color: #409EFF;
+            }
+            .el-icon-arrow-down {
+                font-size: 12px;
+            }
+        }
+    }
+</style>
